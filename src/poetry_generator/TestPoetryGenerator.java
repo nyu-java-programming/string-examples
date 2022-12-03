186c1db6c168896d5e3b4993e74dee03039406d0
bloombar <amos@cs.nyu.edu>
2022-12-02 21:57:40 -0500

    making repo

---
+++ src/poetry_generator/TestPoetryGenerator.java
@@ -0,0 +1,21 @@
+package poetry_generator;
+
+/**
+ * Make a poem using the PoetryGenerator class.
+ * @author Foo Barstein
+ * @version 2
+ */
+public class TestPoetryGenerator {
+
+	public static void main(String[] args) {
+
+		// instantiate a poetry generator
+		PoetryGenerator g = new PoetryGenerator();
+
+		// get a ten-line poem.
+		String poem = g.getAPoem(2); 
+		System.out.println(poem);
+
+	}
+
+}
